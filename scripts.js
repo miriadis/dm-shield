a6a400a4256c175a26790762ee7667b1dc312d20
Rafael Masoni <rafaelmasoni@gmail.com>
2019-07-16 06:53:08 -0300

    Add ft/lb converters, add hypotenuse calculator, fix copy and comments

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-/* toggleAttr function by @mathiasbynens */
+// toggleAttr function by @mathiasbynens
 
 jQuery.fn.toggleAttr = function(attr){
   return this.each(function(){
@@ -8,11 +8,10 @@ jQuery.fn.toggleAttr = function(attr){
 };
 
 
-// CR Search
-
 $(document).ready(function(){
 
-  // When selects change */
+  // CR search
+  // When selects change
   $('[data-cr-min], [data-cr-max]').on('change', function(){
     // If the split CR checkbox isn't checked
     if(!$('[data-cr-split]').prop('checked')){
@@ -28,6 +27,37 @@ $(document).ready(function(){
     $('[data-cr-hidden]').toggleAttr('disabled');
   });
 
+  // Feet calculator
+  // When input changes
+  $('[data-feet-ft]').on('change keyup', function(){
+    var ft = $('[data-feet-ft]').val();
+    var m = (ft*0.3048).toFixed(1);
+    var sq = (Math.floor(ft/5)/5)*5;
+    $('[data-feet-m]').val(m);
+    $('[data-feet-sq]').val(sq);
+  });
+
+  // Pound calculator
+  // When input changes
+  $('[data-pound-lb]').on('change keyup', function(){
+    var lb = $('[data-pound-lb]').val();
+    var kg = (lb*0.45).toFixed(1);
+    $('[data-pound-kg]').val(kg);
+  });
+
+  // Hypotenuse calculator
+  // When inputs change
+  $('[data-hypotenuse-x], [data-hypotenuse-y]').on('change keyup', function(){
+    var a = $('[data-hypotenuse-x]').val();
+    var b = $('[data-hypotenuse-y]').val();
+    // Calculate square root of a^2+b^2, and round to nearest multiple of 5
+    var c = Math.round(Math.sqrt(a*a+b*b)/5)*5;
+    if($(this).val()){
+      $('[data-hypotenuse-result]').val(c);
+    };
+  });
+
+  // Tooltips
   $('[title]').tooltipster({
     animation: 'grow',
     delay: 100,
